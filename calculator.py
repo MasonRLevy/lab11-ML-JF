5ba15b892ea6014682acf3d5a92e2ca976efc1fa
fauble-jaiden <fauble.jaiden@ufl.edu>
2025-11-12 20:13:43 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -4,9 +4,8 @@ def add(a, b): return a + b
 
 def square_root(a):
     if a < 0:
-        raise[ValueError]
-    else:
-        return math.sqrt(a)
+        raise ValueError
+    return math.sqrt(a)
 def hypotenuse(a, b):
     return math.hypot(a, b)
 
@@ -18,13 +17,12 @@ def mul(a, b): return a * b
 
 def div(a, b):
     if a == 0:
-        raise[ZeroDivisionError]
-    else:
-        return b / a
+        raise ZeroDivisionError
+    return b/a
 
 def log(a, b):
     if b <= 0:
-        raise[ValueError]
+        raise ValueError
     else:
         return math.log(b, a)
 
